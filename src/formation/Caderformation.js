883e9ba66153eb4d826cbd861bb8e3182de88f3a
Yosriii <yosri_amri@yahoo.fr>
2018-12-24 12:10:14 +0100

    formation update

--- src/formation/Caderformation.js
+++ src/formation/Caderformation.js
@@ -4,25 +4,39 @@ import styled from "styled-components" ;
 const Wrapper = styled.div`
 .formation-item{
     border: 3px solid black;
-    margin-bottom: 10px;
+    display:flex;
+    justify-content: space-around;
+    align-items: center ;
+    text-align: baseline ;
+    margin-bottom: 30px ;
   }
-  .formation-item{
-      display:flex;
-      justify-content: space-around;
-  }
+.formation-item-content{
+    display: flex ;
+    flex-direction: column ;
+    align-items: center ;
+    width: 20% ;
+    max-width: 20% ;
+}
+.formation-item-header{
+    display: flex ;
+    align-items: center ;
+}
+
+
+
 `
         
 const CardFormation= ({card})=>{
     return (
         <Wrapper>
-        <div className=" col-md-8">
+            
             <div className="formation-item">
                 <div className="formation-item-header">
                 <p>PHOTO</p>
                     <img src="images/logo/20170525033544_2ec25344ad50b41db8060e3d4f9e3840.png"  alt=""/>
                     
                 </div>
-                <div className="formation-item-content">
+                <div  className="formation-item-content">
                 <div className="formation-item-header-info">
                         <h5 className="formation-item-title">{card.title}</h5>
                         
@@ -31,8 +45,9 @@ const CardFormation= ({card})=>{
                         <a href="/#">Voir détails ></a>
                     </div>
                 </div>
+        
             </div>
-        </div>
+        
         </Wrapper>
     )
 }
